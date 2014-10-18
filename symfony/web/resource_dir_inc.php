866c36ce0e7465839d8dd67c133d9672edd94564
Atet Agua <atetme@gmail.com>
2014-10-18 14:31:27 +0800

    Initial commit to local repo

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_5423f414f07668.51610461/css');
+sfConfig::set('sf_web_js_dir_name','webres_5423f414f07668.51610461/js');
+sfConfig::set('sf_web_images_dir_name','webres_5423f414f07668.51610461/images');
+sfConfig::set('ohrm_resource_dir', 'webres_5423f414f07668.51610461');
